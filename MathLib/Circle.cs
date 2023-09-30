214b7bd050d0fbadc9c92cf06071cdd917eecec7
Nikita <zalesov.anatoly@yandex.ru>
2023-09-30 22:20:44 +0400

    Added C# part with tests

---
+++ MathLib/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    internal class Circle : FigureFactory
+    {
+        public double Radius { get; private set; }
+
+        public Circle(string FigureName, double Radius) : base(FigureName)
+        {
+            this.Radius = Radius;
+        }
+
+        public override double CalcSquare()
+        {
+            return Math.Round(Math.PI * Math.Pow(Radius, 2), 1);
+        }
+    }
+}
---
+++ MathLib/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle("Circle", 12);
+            Triangle triangle = new Triangle("Triangle", 4, 4, 5);
+
+            Console.WriteLine(circle.CalcSquare());
+            Console.WriteLine(triangle.CalcSquare());
+        }
+    }
+}
