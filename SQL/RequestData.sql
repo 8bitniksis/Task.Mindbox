f46cfc6efc3fe6491aab31841065c0ffd0feb1fa
Nikita <57682184+8bitniksis@users.noreply.github.com>
2023-11-12 12:13:19 +0400

    Update RequestData.sql

--- SQL/RequestData.sql
+++ SQL/RequestData.sql
@@ -1,4 +1,6 @@
-SELECT p.name AS product, c.name AS category FROM Products AS p
-LEFT JOIN ProdCat AS pc ON p.id = pc.products_id
-INNER JOIN Category AS c ON c.id = pc.category_id
-ORDER BY product;
+SELECT P."Name", C."Name"
+FROM Products P
+LEFT JOIN ProductCategories PC
+ON P.Id = PC.ProductId
+LEFT JOIN Categories C
+ON PC.CategoryId = C.Id;
